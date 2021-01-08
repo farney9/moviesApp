0bb26d13d6e16e9db1778a23777dbb255bf87c79
Farney Jiménez Toro <farney9@gmail.com>
2021-01-07 20:13:41 -0500

    animated stykes

--- src/app/pages/movie/movie.component.ts
+++ src/app/pages/movie/movie.component.ts
@@ -1,6 +1,7 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { combineLatest } from 'rxjs';
 import { Cast, Credits } from 'src/app/interfaces/credits';
 import { MovieDetails } from 'src/app/interfaces/movie-details';
 import { PeliculasService } from 'src/app/services/peliculas.service';
@@ -25,26 +26,37 @@ export class MovieComponent implements OnInit {
     
     // otra forma de extraer el id es através de la des estructuración
     const {id} =  this.activatedRoute.snapshot.params;
-    
-    this.peliculasService.getMovieDetails(id).subscribe(movie => {
+
+    combineLatest([
+      this.peliculasService.getMovieDetails(id),
+      this.peliculasService.getCredits(id)
+
+    ]).subscribe(([movie, cast]) => {
+      // console.log(movie, cast);
       if (!movie) {
         this.router.navigateByUrl('/home');
         return;
       }
       // console.log(movie);
-      this.pelicula = movie;
-    });
-    
-    this.peliculasService.getCredits(id).subscribe( cast => {
-      if (!cast) {
-        this.router.navigateByUrl('/home');
-        return;
-      }
-      
+      this.pelicula = movie;      
       this.cast = cast;
-      console.log(cast);
+
+    })
+    
+    // this.peliculasService.getMovieDetails(id).subscribe(movie => {
+      // if (!movie) {
+      //   this.router.navigateByUrl('/home');
+      //   return;
+      // }
+      // // console.log(movie);
+      // this.pelicula = movie;
+    // });
+    
+    // this.peliculasService.getCredits(id).subscribe( cast => {
+    //   this.cast = cast;
+    //   console.log(cast);
       
-    });
+    // });
   }
 
   onReturn(){
